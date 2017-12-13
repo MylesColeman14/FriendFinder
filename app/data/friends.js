96fbb1408b51e1052403f4b6483bc4f892f77450
Myles Coleman <myles.coleman14@gmail.com>
2017-12-12 18:38:33 -0600

    almost there

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Donald Glover',
+		photo: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzUxNTU5ODkxNl5BMl5BanBnXkFtZTgwOTIyNjc5MDI@._V1_UX214_CR0,0,214,317_AL_.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kendrick Lamar',
+		photo: 'http://cache.umusic.com/_sites/kendricklamar.com/images/og.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'J Cole',
+		photo: 'http://hiphopspeakeasy.com/wp-content/uploads/2017/02/j-cole-high-for-hours-2-1700x1080.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Drake',
+		photo: 'https://www.biography.com/.image/t_share/MTQ3NTI2OTA4NzY5MjE2MTI4/drake_photo_by_prince_williams_wireimage_getty_479503454.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Lil Yachty',
+		photo: 'http://img.wennermedia.com/article-leads-horizontal/lil-yatchy-08c5fec5-c39b-4999-907b-15b5d7b88b18.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Lil pump',
+		photo: 'https://images.complex.com/complex/images/c_scale,w_1100/fl_lossy,pg_1,q_auto/eikspg9z4w9diypmkboh/lil-pump-4',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Adele Adkins',
+		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Lil Wayne',
+		photo: 'https://yt3.ggpht.com/-UnYQfqfdfts/AAAAAAAAAAI/AAAAAAAAAAA/aeJj-lizNSA/s900-c-k-no-mo-rj-c0xffffff/photo.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Kodack Black',
+		photo: 'https://www.billboard.com/files/media/Kodak-Black-Press-Photo-cr-David-Cabrera-2016-billboard-1548.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
