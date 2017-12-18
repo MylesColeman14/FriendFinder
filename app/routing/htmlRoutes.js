134160af8f8c86b9a2cda7672b53b66ac4ee49ba
Myles Coleman <myles.coleman14@gmail.com>
2017-12-18 17:55:01 -0600

    closer

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,15 @@
-// Pull in required dependencies
+
 var path = require('path');
 
-// Export HTML routes
-module.exports = function(app) {
-	// console.log('___ENTER htmlRoutes.js___');
 
-	// Home page
+module.exports = function(app) {
+
+
+
 	app.get('/', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 
-	// Survey page
 	app.get('/survey', function(req, res) {
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
 	});
